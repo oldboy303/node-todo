532c57d007a7be7259e1825b7810b4176bcd6723
San Guyer <sanguyer@gmail.com>
2017-12-05 22:49:04 -0500

    Initial setup of APIs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
 var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 
 var port = process.env.PORT || 3000;
 
@@ -12,6 +13,7 @@ app.use('/assets', express.static(__dirname + '/public'));
 
 mongoose.connect(config.getDbConnectionString());
 setupController(app);
+apiController(app);
 
 app.listen(port);
  
